2d7a2e98d2e61c576a363185b1f76b48ee0cb7bd
Robin <me@robin.com.au>
2018-11-22 23:51:08 +0800

    onCreate, onUpdate and onCrup working correctly finally hopefully.

--- Mvil.kt
+++ Mvil.kt
@@ -24,17 +24,35 @@ fun render(activity: Activity, rootView: ViewGroup, virtuals: List<Virtual>) {
         rootView,
         virtuals)
     realChildren.zip(virtuals).forEachIndexed {i, (realChildOrNull, virtualChild) ->
-        val realChild: View = if (realChildOrNull == null) {
-            realiseView(activity, rootView, virtualChild, i)
+        val (realChild, justCreated) = if (realChildOrNull == null) {
+            Pair(realiseView(activity, rootView, virtualChild, i), true)
         } else {
             reOrderChild(realChildOrNull, i)
-            realChildOrNull
+            Pair(realChildOrNull, false)
         }
         val newTag = virtualChild.tag
         val cachedTag = getViewCache(realChild, "tag")
         if (cachedTag == null) {
             val (key, tagF) = tag(newTag)
             tagF(realChild)
+        }
+        val attrsMap = virtualChild.attrs.associate {(key, viewF) ->
+            key to viewF
+        }
+        val onCreate = attrsMap.get("onCreate")
+        val onUpdate = attrsMap.get("onUpdate")
+        val onCrup = attrsMap.get("onCrup")
+        if (justCreated) {
+            if (onCreate != null) {
+                onCreate(realChild)
+            }
+        } else {
+            if (onUpdate != null) {
+                onUpdate(realChild)
+            }
+        }
+        if (onCrup != null) {
+            onCrup(realChild)
         }
         for ((key, f) in virtualChild.attrs) {
             f(realChild)
